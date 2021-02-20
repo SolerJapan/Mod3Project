16f108a93af67d62f6e7042e57dca386d209ae4b
Alex Sakata <solerjapan@gmail.com>
2021-02-20 00:52:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,26 +7,6 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-
-    this.state = { 
-      username: '',
-      password: ''        
-    };
-                
-    //this.submitForm = this.submitForm.bind(this);
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(event) {
-    // Will capture the input value on the <input> tag and save into state 
-    // Once the form is submitted, the setstate on the function will reset the value to an empty string
-
-    this.setState({[event.target.name]: event.target.value})
-   
-  }
-
   render(){
 
   return (
