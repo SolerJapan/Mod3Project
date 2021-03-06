c1c5ad70d8777a40e548c8417e190e76d85bbd87
Alex Sakata <solerjapan@gmail.com>
2021-03-06 18:25:02 -0500

    Update Constants.js

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,2 +1,8 @@
-export const API_URL = 'http://localhost:8081'
-export const JPA_API_URL = 'http://localhost:8081/jpa'
+//for heroku
+export const API_URL = ''
+export const JPA_API_URL = '/jpa'
+
+
+//for local
+// export const API_URL = 'http://localhost:8081'
+// export const JPA_API_URL = 'http://localhost:8081/jpa'
